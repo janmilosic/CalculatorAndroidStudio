b7f45fd5d0f1c6cb64686165963cd5739d1be801
AnejP <predovnik.anej@gmail.com>
2022-01-18 17:05:29 +0100

    Še malo boljše

--- app/src/main/java/com/example/prva4a22/MainActivity.java
+++ app/src/main/java/com/example/prva4a22/MainActivity.java
@@ -8,12 +8,6 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button clear, equals;
-    Button add, subtract, divide, multipy;
-
-    String niz1;
-
     EditText vnos;
 
     @Override
@@ -22,49 +16,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         vnos = findViewById(R.id.vnos);
-
-        clear = findViewById(R.id.clearBTN);
-        equals = findViewById(R.id.equalsBTN);
-
-        add = findViewById(R.id.addBTN);
-        subtract = findViewById(R.id.subtractBTN);
-        multipy = findViewById(R.id.multiplyBTN);
-        divide = findViewById(R.id.divideBTN);
-
     }
 
-    public void addNumber(View v){
+    public void addText(View v){
         String s = String.valueOf(vnos.getText());
         vnos.setText(s + ((Button)v).getText().toString());
     }
 
     public void Clear(View v){
-        String s = String.valueOf(vnos.getText());
         vnos.setText("");
     }
-
-
-
-    public void Add(View v){
-        String add = String.valueOf(vnos.getText());
-        niz1 = String.valueOf(vnos.getText());
-        vnos.setText(add + "+");
-    }
-
-    public void Subtract(View v){
-        String subtract = String.valueOf(vnos.getText());
-        vnos.setText(subtract + "-");
-    }
-
-    public void Multipy(View v){
-        String multiply = String.valueOf(vnos.getText());
-        vnos.setText(multiply + "×");
-    }
-
-    public void Divide(View v){
-        String divide = String.valueOf(vnos.getText());
-        vnos.setText(divide + "÷");
-    }
-
-
 }
