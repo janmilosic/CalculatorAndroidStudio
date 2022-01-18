d0032458a059424a0e91d67113bbcec2102ff88a
Anej <66727079+AnejP@users.noreply.github.com>
2022-01-18 16:47:53 +0100

    Malo fejt bolj efficient (#1)

--- app/src/main/java/com/example/prva4a22/MainActivity.java
+++ app/src/main/java/com/example/prva4a22/MainActivity.java
@@ -9,7 +9,6 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button one, two, three, four, five, six, seven, eight, nine, zero;
     Button clear, equals;
     Button add, subtract, divide, multipy;
 
@@ -22,16 +21,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        one = findViewById(R.id.oneBTN);
-        two = findViewById(R.id.twoBTN);
-        three = findViewById(R.id.threeBTN);
-        four = findViewById(R.id.fourBTN);
-        five = findViewById(R.id.fiveBTN);
-        six = findViewById(R.id.sixBTN);
-        seven = findViewById(R.id.sevenBTN);
-        eight = findViewById(R.id.eightBTN);
-        nine = findViewById(R.id.nineBTN);
-        zero = findViewById(R.id.zeroBTN);
         vnos = findViewById(R.id.vnos);
 
         clear = findViewById(R.id.clearBTN);
@@ -44,54 +33,9 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void One(View v){
+    public void addNumber(View v){
         String s = String.valueOf(vnos.getText());
-        vnos.setText(s + "1");
-    }
-
-    public void Two(View v){
-        String s = String.valueOf(vnos.getText());
-        vnos.setText(s + "2");
-    }
-
-    public void Three(View v){
-        String s = String.valueOf(vnos.getText());
-        vnos.setText(s + "3");
-    }
-
-    public void Four(View v){
-        String s = String.valueOf(vnos.getText());
-        vnos.setText(s + "4");
-    }
-
-    public void Five(View v){
-        String s = String.valueOf(vnos.getText());
-        vnos.setText(s + "5");
-    }
-
-    public void Six(View v){
-        String s = String.valueOf(vnos.getText());
-        vnos.setText(s + "6");
-    }
-
-    public void Seven(View v){
-        String s = String.valueOf(vnos.getText());
-        vnos.setText(s + "7");
-    }
-
-    public void Eight(View v){
-        String s = String.valueOf(vnos.getText());
-        vnos.setText(s + "8");
-    }
-
-    public void Nine(View v){
-        String s = String.valueOf(vnos.getText());
-        vnos.setText(s + "9");
-    }
-
-    public void Zero(View v){
-        String s = String.valueOf(vnos.getText());
-        vnos.setText(s + "0");
+        vnos.setText(s + ((Button)v).getText().toString());
     }
 
     public void Clear(View v){
