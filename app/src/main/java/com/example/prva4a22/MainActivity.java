9cb3dedfe99b9492325b95bad956128a755ca8cf
milosic.jan1 <milosic.jan1@scptuj.si>
2022-02-02 13:42:37 +0100

    nova verzija ne dela!

--- app/src/main/java/com/example/prva4a22/MainActivity.java
+++ app/src/main/java/com/example/prva4a22/MainActivity.java
@@ -1,6 +1,219 @@
 package com.example.prva4a22;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    Button one;
+    Button two;
+    Button three;
+    Button four;
+    Button five;
+    Button six;
+    Button seven;
+    Button eight;
+    Button nine;
+    Button zero;
+    Button clear;
+    Button devide;
+    Button times;
+    Button minus;
+    Button plus;
+    Button equal;
+    EditText vnos;
+    Double prvast = 0.0;
+    String operacija;
+    Boolean napaka = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        one = findViewById(R.id.one);
+        two = findViewById(R.id.two);
+        three = findViewById(R.id.three);
+        four = findViewById(R.id.four);
+        five = findViewById(R.id.five);
+        six = findViewById(R.id.six);
+        seven = findViewById(R.id.seven);
+        eight = findViewById(R.id.eight);
+        nine = findViewById(R.id.nine);
+        zero = findViewById(R.id.zero);
+        clear = findViewById(R.id.clear);
+        devide = findViewById(R.id.devide);
+        times = findViewById(R.id.times);
+        minus = findViewById(R.id.minus);
+        plus = findViewById(R.id.plus);
+        equal = findViewById(R.id.equal);
+
+        vnos = findViewById(R.id.vnos);
+
+    }
+
+    public void one(View v){
+        if(napaka==true) {
+            vnos.setText("");
+            napaka = false;
+        }
+        String s = String.valueOf(vnos.getText());
+        vnos.setText(s+"1");
+    }
+    public void two(View v){
+        if(napaka==true){
+            vnos.setText("");
+            napaka=false;
+        }
+        String s = String.valueOf(vnos.getText());
+        vnos.setText(s+"2");
+    }
+    public void three(View v){
+        if(napaka==true){
+            vnos.setText("");
+            napaka=false;
+        }
+        String s = String.valueOf(vnos.getText());
+        vnos.setText(s+"3");
+    }
+    public void four(View v){
+        if(napaka==true){
+            vnos.setText("");
+            napaka=false;
+        }
+        String s = String.valueOf(vnos.getText());
+        vnos.setText(s+"4");
+    }
+    public void five(View v){
+        if(napaka==true){
+            vnos.setText("");
+            napaka=false;
+        }
+        String s = String.valueOf(vnos.getText());
+        vnos.setText(s+"5");
+    }
+    public void six(View v){
+        if(napaka==true){
+            vnos.setText("");
+            napaka=false;
+        }
+        String s = String.valueOf(vnos.getText());
+        vnos.setText(s+"6");
+    }
+    public void seven(View v){
+        if(napaka==true){
+            vnos.setText("");
+            napaka=false;
+        }
+        String s = String.valueOf(vnos.getText());
+        vnos.setText(s+"7");
+    }
+    public void eight(View v){
+        if(napaka==true){
+            vnos.setText("");
+            napaka=false;
+        }
+        String s = String.valueOf(vnos.getText());
+        vnos.setText(s+"8");
+    }
+    public void nine(View v){
+        if(napaka==true){
+            vnos.setText("");
+            napaka=false;
+        }
+        String s = String.valueOf(vnos.getText());
+        vnos.setText(s+"9");
+    }
+    public void zero(View v){
+        if(napaka==true){
+            vnos.setText("");
+            napaka=false;
+        }
+        String s = String.valueOf(vnos.getText());
+        vnos.setText(s+"0");
+    }
+    public void brisi(View v) {
+        vnos.setText("");
+    }
+
+    public void nega(View v){
+        String s = String.valueOf(vnos.getText());
+        vnos.setText("-"+s);
+    }
+    public void plus(View v){
+        try{
+            prvast=Double.valueOf(vnos.getText().toString());
+            vnos.setText("");
+            operacija="plus";
+        }catch (NumberFormatException e){
+            vnos.setText("Error");
+            napaka=true;
+        }
+    }
+    public void minus(View v){
+        try{
+            prvast=Double.valueOf(vnos.getText().toString());
+            vnos.setText("");
+            operacija="minus";
+        }catch (NumberFormatException e){
+            vnos.setText("Error");
+            napaka=true;
+        }
+
+    }
+    public void times(View v){
+        try{
+            prvast=Double.valueOf(vnos.getText().toString());
+            vnos.setText("");
+            operacija="times";
+        }catch (NumberFormatException e){
+            vnos.setText("Error");
+            napaka=true;
+        }
+
+    }
+    public void devide(View v){
+        try{
+            prvast=Double.valueOf(vnos.getText().toString());
+            vnos.setText("");
+            operacija="devide";
+        }catch (NumberFormatException e){
+            vnos.setText("Error");
+            napaka=true;
+        }
+
+    }
+
+    public void enako(View v) {
+        switch (operacija) {
+            case "plus":
+                Double r = prvast + Double.valueOf(vnos.getText().toString());
+                vnos.setText(r.toString());
+                break;
+            case "minus":
+                Double a = prvast - Double.valueOf(vnos.getText().toString());
+                vnos.setText(a.toString());
+                break;
+            case "times":
+                Double b = prvast * Double.valueOf(vnos.getText().toString());
+                vnos.setText(b.toString());
+                break;
+            case "devide":
+                Double c = prvast / Double.valueOf(vnos.getText().toString());
+                vnos.setText(c.toString());
+                break;
+
+        }
+    }
+}
+
+
+/*
+package com.example.prva4a22;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
@@ -127,3 +340,5 @@ public class MainActivity extends AppCompatActivity {
         }.parse();
     }
 }
+
+ */
