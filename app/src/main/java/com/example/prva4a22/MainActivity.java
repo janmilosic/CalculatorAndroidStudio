05a9edfbec3e28655e518db12c5d00b4cadc82ef
Jan Milosic <61479658+janmilosic@users.noreply.github.com>
2022-02-09 01:09:27 +0100

    popravki in nov nacin delovanja (vse dela)

--- app/src/main/java/com/example/prva4a22/MainActivity.java
+++ app/src/main/java/com/example/prva4a22/MainActivity.java
@@ -1,139 +1,64 @@
 package com.example.prva4a22;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
 import android.view.View;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
-    Button one;
-    Button two;
-    Button three;
-    Button four;
-    Button five;
-    Button six;
-    Button seven;
-    Button eight;
-    Button nine;
-    Button zero;
-    Button clear;
-    Button devide;
-    Button times;
-    Button minus;
-    Button plus;
-    Button equal;
-    EditText vnos;
-    Double prvast = 0.0;
-    String operacija;
-    Boolean napaka = false;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button clear, divide, multiply, minus, plus, equal, procent, sqrt, decimal;
 
+    Double prvast = 0.0;
+    Boolean errors = false;
+    String racunska_operacija;
+
+    private EditText vnos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        one = findViewById(R.id.one);
-        two = findViewById(R.id.two);
-        three = findViewById(R.id.three);
-        four = findViewById(R.id.four);
-        five = findViewById(R.id.five);
-        six = findViewById(R.id.six);
-        seven = findViewById(R.id.seven);
-        eight = findViewById(R.id.eight);
-        nine = findViewById(R.id.nine);
-        zero = findViewById(R.id.zero);
-        clear = findViewById(R.id.clear);
-        devide = findViewById(R.id.devide);
-        times = findViewById(R.id.times);
-        minus = findViewById(R.id.minus);
-        plus = findViewById(R.id.plus);
-        equal = findViewById(R.id.equal);
-
         vnos = findViewById(R.id.vnos);
 
+        Button button0 = findViewById(R.id.zeroBTN);
+        Button button1 = findViewById(R.id.oneBTN);
+        Button button2 = findViewById(R.id.twoBTN);
+        Button button3 = findViewById(R.id.threeBTN);
+        Button button4 = findViewById(R.id.fourBTN);
+        Button button5 = findViewById(R.id.fiveBTN);
+        Button button6 = findViewById(R.id.sixBTN);
+        Button button7 = findViewById(R.id.sevenBTN);
+        Button button8 = findViewById(R.id.eightBTN);
+        Button button9 = findViewById(R.id.nineBTN);
+
+        button0.setOnClickListener(this);
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+        button4.setOnClickListener(this);
+        button5.setOnClickListener(this);
+        button6.setOnClickListener(this);
+        button7.setOnClickListener(this);
+        button8.setOnClickListener(this);
+        button9.setOnClickListener(this);
+
+        clear = findViewById(R.id.clearBTN);
+        divide = findViewById(R.id.divideBTN);
+        multiply = findViewById(R.id.multiplyBTN);
+        minus = findViewById(R.id.subtractBTN);
+        plus = findViewById(R.id.addBTN);
+        equal = findViewById(R.id.equalsBTN);
+        vnos = findViewById(R.id.vnos);
+        procent = findViewById(R.id.procentBTN);
+        sqrt = findViewById(R.id.sqrtBTN);
+        decimal = findViewById(R.id.decimalBTN);
+
     }
 
-    public void one(View v){
-        if(napaka==true) {
-            vnos.setText("");
-            napaka = false;
-        }
-        String s = String.valueOf(vnos.getText());
-        vnos.setText(s+"1");
-    }
-    public void two(View v){
-        if(napaka==true){
-            vnos.setText("");
-            napaka=false;
-        }
-        String s = String.valueOf(vnos.getText());
-        vnos.setText(s+"2");
-    }
-    public void three(View v){
-        if(napaka==true){
-            vnos.setText("");
-            napaka=false;
-        }
-        String s = String.valueOf(vnos.getText());
-        vnos.setText(s+"3");
-    }
-    public void four(View v){
-        if(napaka==true){
-            vnos.setText("");
-            napaka=false;
-        }
-        String s = String.valueOf(vnos.getText());
-        vnos.setText(s+"4");
-    }
-    public void five(View v){
-        if(napaka==true){
-            vnos.setText("");
-            napaka=false;
-        }
-        String s = String.valueOf(vnos.getText());
-        vnos.setText(s+"5");
-    }
-    public void six(View v){
-        if(napaka==true){
-            vnos.setText("");
-            napaka=false;
-        }
-        String s = String.valueOf(vnos.getText());
-        vnos.setText(s+"6");
-    }
-    public void seven(View v){
-        if(napaka==true){
-            vnos.setText("");
-            napaka=false;
-        }
-        String s = String.valueOf(vnos.getText());
-        vnos.setText(s+"7");
-    }
-    public void eight(View v){
-        if(napaka==true){
-            vnos.setText("");
-            napaka=false;
-        }
-        String s = String.valueOf(vnos.getText());
-        vnos.setText(s+"8");
-    }
-    public void nine(View v){
-        if(napaka==true){
-            vnos.setText("");
-            napaka=false;
-        }
-        String s = String.valueOf(vnos.getText());
-        vnos.setText(s+"9");
-    }
-    public void zero(View v){
-        if(napaka==true){
-            vnos.setText("");
-            napaka=false;
-        }
-        String s = String.valueOf(vnos.getText());
-        vnos.setText(s+"0");
-    }
     public void brisi(View v) {
         vnos.setText("");
     }
@@ -146,48 +71,79 @@ public class MainActivity extends AppCompatActivity {
         try{
             prvast=Double.valueOf(vnos.getText().toString());
             vnos.setText("");
-            operacija="plus";
+            racunska_operacija="plus";
         }catch (NumberFormatException e){
             vnos.setText("Error");
-            napaka=true;
+            errors=true;
         }
     }
     public void minus(View v){
         try{
             prvast=Double.valueOf(vnos.getText().toString());
             vnos.setText("");
-            operacija="minus";
+            racunska_operacija="minus";
         }catch (NumberFormatException e){
             vnos.setText("Error");
-            napaka=true;
+            errors=true;
         }
 
     }
-    public void times(View v){
+    public void multiply(View v){
         try{
             prvast=Double.valueOf(vnos.getText().toString());
             vnos.setText("");
-            operacija="times";
+            racunska_operacija="multiply";
         }catch (NumberFormatException e){
             vnos.setText("Error");
-            napaka=true;
+            errors=true;
         }
 
     }
-    public void devide(View v){
+    public void divide(View v){
         try{
             prvast=Double.valueOf(vnos.getText().toString());
             vnos.setText("");
-            operacija="devide";
+            racunska_operacija="divide";
         }catch (NumberFormatException e){
             vnos.setText("Error");
-            napaka=true;
+            errors=true;
         }
 
     }
+    public void procent(View v){
+        try{
+            prvast=Double.valueOf(vnos.getText().toString());
+            vnos.setText("");
+            racunska_operacija="procent";
+        }catch (NumberFormatException e){
+            vnos.setText("Error");
+            errors=true;
+        }
+
+    }
+    public void sqrt(View v){
+        try{
+            prvast=Double.valueOf(vnos.getText().toString());
+            vnos.setText("");
+            racunska_operacija="sqrt";
+        }catch (NumberFormatException e){
+            vnos.setText("Error");
+            errors=true;
+        }
+
+    }
+        public void decimal(View v){
+            if(errors==true){
+                vnos.setText("");
+                errors=false;
+            }
+            String s = String.valueOf(vnos.getText());
+            vnos.setText(s+".");
+        }
+
 
     public void enako(View v) {
-        switch (operacija) {
+        switch (racunska_operacija) {
             case "plus":
                 Double r = prvast + Double.valueOf(vnos.getText().toString());
                 vnos.setText(r.toString());
@@ -196,21 +152,118 @@ public class MainActivity extends AppCompatActivity {
                 Double a = prvast - Double.valueOf(vnos.getText().toString());
                 vnos.setText(a.toString());
                 break;
-            case "times":
+            case "multiply":
                 Double b = prvast * Double.valueOf(vnos.getText().toString());
                 vnos.setText(b.toString());
                 break;
-            case "devide":
+            case "divide":
                 Double c = prvast / Double.valueOf(vnos.getText().toString());
                 vnos.setText(c.toString());
+                break;
+            case "procent":
+                Double d = prvast / 100;
+                vnos.setText(d.toString());
+                break;
+            case "sqrt":
+                Double e = Math.sqrt(prvast);
+                vnos.setText(e.toString());
                 break;
 
         }
     }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.zeroBTN:
+                if(errors==true) {
+                    vnos.setText("");
+                    errors = false;
+                }
+                String a = String.valueOf(vnos.getText());
+                vnos.setText(a+"0");
+                break;
+            case R.id.oneBTN:
+                if(errors==true) {
+                    vnos.setText("");
+                    errors = false;
+                }
+                String b = String.valueOf(vnos.getText());
+                vnos.setText(b+"1");
+                break;
+            case R.id.twoBTN:
+                if(errors==true) {
+                    vnos.setText("");
+                    errors = false;
+                }
+                String c = String.valueOf(vnos.getText());
+                vnos.setText(c+"2");
+                break;
+            case R.id.threeBTN:
+                if(errors==true) {
+                    vnos.setText("");
+                    errors = false;
+                }
+                String d = String.valueOf(vnos.getText());
+                vnos.setText(d+"3");
+                break;
+            case R.id.fourBTN:
+                if(errors==true) {
+                    vnos.setText("");
+                    errors = false;
+                }
+                String e = String.valueOf(vnos.getText());
+                vnos.setText(e+"4");
+                break;
+            case R.id.fiveBTN:
+                if(errors==true) {
+                    vnos.setText("");
+                    errors = false;
+                }
+                String f = String.valueOf(vnos.getText());
+                vnos.setText(f+"5");
+                break;
+            case R.id.sixBTN:
+                if(errors==true) {
+                    vnos.setText("");
+                    errors = false;
+                }
+                String g = String.valueOf(vnos.getText());
+                vnos.setText(g+"6");
+                break;
+            case R.id.sevenBTN:
+                if(errors==true) {
+                    vnos.setText("");
+                    errors = false;
+                }
+                String h = String.valueOf(vnos.getText());
+                vnos.setText(h+"7");
+                break;
+            case R.id.eightBTN:
+                if(errors==true) {
+                    vnos.setText("");
+                    errors = false;
+                }
+                String i = String.valueOf(vnos.getText());
+                vnos.setText(i+"8");
+                break;
+            case R.id.nineBTN:
+                if(errors==true) {
+                    vnos.setText("");
+                    errors = false;
+                }
+                String j = String.valueOf(vnos.getText());
+                vnos.setText(j+"9");
+                break;
+        }
+    }
 }
 
-
 /*
+
+
+STARA KODA UPORABA EVAL FUNKCIJE
+
 package com.example.prva4a22;
 
 import androidx.appcompat.app.AppCompatActivity;
